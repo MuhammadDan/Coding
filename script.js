0b8aca0826bf3c719a9a63058e1c6af502c7007a
MuhammadDan <daniyalk154330@gmail.com>
2025-12-08 11:24:57 +0500

    This is first code

---
+++ script.js
@@ -0,0 +1,11 @@
+function largestnumber(arr) {
+    let largest = 0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>largest){
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+var arr = [2, 5, 1, 8, 3];
+console.log(largestnumber(arr));
